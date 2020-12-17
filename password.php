94d5402b17f329344ac6ad33c9073c9bbd1b7a97
John J Kauflin <johnkauflin@gmail.com>
2020-12-17 11:26:56 -0500

    Corrected seperator bug

--- password.php
+++ password.php
@@ -7,19 +7,21 @@
  * Modification History
  * 2020-08-03 JJK 	Initial version
  * 2020-12-15 JJK   Update for package
+ * 2020-12-17 JJK   Corrected separator bug
  *============================================================================*/
+// Define a super global constant for the log file (this will be in scope for all functions)
+define("LOG_FILE", "./php.log");
 // Figure out how many levels up to get to the "public_html" root folder
-$webRootDirOffset = substr_count(strstr(dirname(__FILE__),"public_html"),"\\") + 1;
+$webRootDirOffset = substr_count(strstr(dirname(__FILE__),"public_html"),DIRECTORY_SEPARATOR) + 1;
 // Assume /vendor is 3 levels up from a file in the package root
-require_once dirname(__FILE__, 3).'\autoload.php';
+require_once dirname(__FILE__, 3) . DIRECTORY_SEPARATOR . 'autoload.php';
 // Get settings and credentials from a file in a directory outside of public_html
 // (assume a settings file in the "external_includes" folder one level up from "public_html"
-require_once dirname(__FILE__, $webRootDirOffset+1).'/external_includes/jjkloginSettings.php';
+$settingsFile = dirname(__FILE__, $webRootDirOffset+1).DIRECTORY_SEPARATOR.'external_includes'.DIRECTORY_SEPARATOR.'jjkloginSettings.php';
+require_once $settingsFile;
 require_once 'commonUtil.php';
 
 use \jkauflin\jjklogin\LoginAuth;
-// Define a super global constant for the log file (this will be in scope for all functions)
-define("LOG_FILE", "./php.log");
 
 try {
     header("Content-Type: application/json; charset=UTF-8");
