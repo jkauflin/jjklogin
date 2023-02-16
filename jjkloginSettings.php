ad0150a627af4eb70357fd28222adf248161675b
John J Kauflin <johnkauflin@gmail.com>
2023-02-15 21:49:43 -0500

    Moved variables to DB

--- jjkloginSettings.php
+++ jjkloginSettings.php
@@ -15,24 +15,4 @@ $passwordJJKLogin = "<set to DB password>";
 $dbnameJJKLogin = "<set to DB name>";
 $dbUserTableNameJJKLogin = "users";
 
-/*
-$cookiePathJJKLogin = '/<set to path of main web site>';
-$cookieNameJJKLogin = 'jjklogin';
-$serverKeyJJKLogin = "<set to a good long random key string value>";
-$scheduledJobKeyJJKLogin = "";
-
-$mailServer = "SMTP server address";
-$mailPort = 0; // port for SMTP server
-$mailUsername = "username for mail account";
-$mailPassword = "password for mail account"; 
-
-$domainUrlJJKLogin = 'https://<set to main web site url>';
-$fromEmailAddressJJKLogin = "<set to FROM address for user registration emails>";
-$adminEmailListJJKLogin = "<set to admin email address>";
-
-// Set to true to re-direct un-authenticated users to the login
-// (default is false - don't automatically take them to the login)
-$autoRedirectJJKLogin = false;
-*/
-
 ?>
